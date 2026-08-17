14e9602d559ed23febc53128f117176f82eee57c
Muneeb Ur Rehman <123630307+muneeb45600@users.noreply.github.com>
2026-08-17 10:56:06 +0500

    fixing generate qr issue

--- attendance_app/lib/screens/qr_display_screen.dart
+++ attendance_app/lib/screens/qr_display_screen.dart
@@ -49,7 +49,7 @@ class _QRDisplayScreenState extends State<QRDisplayScreen> {
     });
 
     try {
-      final result = await ApiService.generateQR(_classInfo!['id'], expectedCount: _classInfo!['expected_count'],);
+      final result = await ApiService.generateQR(_classInfo!['class_info']['id'], expectedCount: _classInfo!['expected_count'],);
       print('QR RESULT: $result');
       if (result['status'] == 201) {
         final data = result['data'];
